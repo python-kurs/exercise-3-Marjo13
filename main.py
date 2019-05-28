29f4b46bd257a029eff627f8f432dce801211c8c
Marjo13 <brehmerj@students.uni-marburg.de>
2019-05-28 13:41:13 +0200

    Update

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 # Exercise 3
 from pathlib import Path
 # import functions from utils here
+import utils as u
 
 data_dir = Path("data")
 output_dir = Path("solution")
 
-# 1. Contstruct the path to the text file in the data directory using the `pathlib` module [2P]
-
+# 1. Contstruct the path to the text file in the data directory 
+# using the `pathlib` module [2P]
+carsp = Path(data_dir/"cars.txt")
 
 # 2. Read the text file [2P]
-
+with open(carsp, "r") as c:
+    cars = c.read()
 
 # 3. Count the occurences of each item in the text file [2P]
 
+#funktioniert nicht
+#Anzahl = u.count(cars, "r")
 
-# 4. Using `pathlib` check if a directory with name `solution` exists and if not create it [2P]
+# 4. Using `pathlib` check if a directory with name `solution` 
+# exists and if not create it [2P]
 
+Path("solution").exists()
 
-# 5. Write the counts to the file `counts.csv` in the `solution` directory in the format (first line is the header): [2P]
+Path("solutions").mkdir(parents=True, exist_ok=True)
+
+# 5. Write the counts to the file `counts.csv` in the `solution` 
+# directory in the format (first line is the header): [2P]
 #    item, count
 #    item_name_1, item_count_1
 #    item_name_2, item_count_2
--- utils.py
+++ utils.py
@@ -2,3 +2,27 @@
 
 # Use this file for all functions you create and might want to reuse later.
 # Import them in the `main.py` script
+
+def count(cars):
+    """
+    Count the occurences of each item in a text file.
+    
+    Parameters
+    ----------
+    
+    file = enter file you want to count
+    
+    Returns
+    -------
+    List with items + number
+    
+    """
+    
+
+    itemcount={}
+    for item in cars:
+        if item  in itemcount.keys():
+            itemcount[item] += 1
+        else:
+            itemcount[item] = 1
+    print (item,itemcount)
